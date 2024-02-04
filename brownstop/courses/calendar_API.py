4e1975e820043e8c27066f1645dd9cc9fe89e55b
Soujanya <soujanya_c_aryal@brown.edu>
2024-02-04 04:14:21 -0500

    Update
    calendar event is pushed to gcal

--- brownstop/courses/calendar_API.py
+++ brownstop/courses/calendar_API.py
@@ -1,8 +1,47 @@
+from decouple import config
+from google.oauth2 import service_account
+import googleapiclient.discovery
+import datetime
+
+CAL_ID = "2d4b40ced10733cff49427eb93b6ef8086e87ce3dcf52e13bbcca385b0446f77@group.calendar.google.com"
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+SERVICE_ACCOUNT_FILE = './google-credentials.json'
+
+
 def test_calendar():
     print("RUNNING TEST_CALENDAR()")
-    test_event1 = {"start": {"date": "2022-01-01"}, "end": {"date": "2022-01-07"}, "summary":"test event 1"}
-    test_event2 = {"start": {"date": "2022-02-01"}, "end": {"date": "2022-02-07"}, "summary":"test event 2"}
-    events = [test_event1, test_event2]
+
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+
+    # CREATE A NEW EVENT
+    new_event = {
+    'summary': "Ben Hammond Tech's Super Awesome Event",
+    'location': 'Denver, CO USA',
+    'description': 'https://benhammond.tech',
+    'start': {
+        'date': f"{datetime.date.today()}",
+        'timeZone': 'America/New_York',
+    },
+    'end': {
+        'date': f"{datetime.date.today() + datetime.timedelta(days=3)}",
+        'timeZone': 'America/New_York',
+    },
+    }
+    service.events().insert(calendarId=CAL_ID, body=new_event).execute()
+    print('Event created')
+
+ # GET ALL EXISTING EVENTS
+    events_result = service.events().list(calendarId=CAL_ID, maxResults=2500).execute()
+    events = events_result.get('items', [])
+
+    # LOG THEM ALL OUT IN DEV TOOLS CONSOLE
+    for e in events:
+        print(e)
+
+    #uncomment the following lines to delete each existing item in the calendar
+    #event_id = e['id']
+        # service.events().delete(calendarId=CAL_ID, eventId=event_id).execute()
+
 
     return events
-
